e74f2d4f93da208a67fa5d943c149a57560a6025
Bourbxn <jakkapat97@gmail.com>
2023-05-03 02:16:18 +0700

    clear client

--- client/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div className="text-5xl text-teal-500">about</div>
-  <router-view></router-view>
-</template>
--- client/src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div className="text-5xl text-teal-500">Home</div>
-  <router-view></router-view>
-</template>
